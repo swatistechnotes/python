2d3a570ede5fdb042caa5c69cabe8f93b7cf9a6e
swatistechnotes <63666256+swatistechnotes@users.noreply.github.com>
2020-04-16 03:26:33 +0530

    Add files via upload

---
+++ bubblesort.py
@@ -0,0 +1,16 @@
+
+
+A = list((5,2,1,3,4,0))
+
+	
+print(A)
+for i in range (len(A)-1) :
+	for j in range ((len(A)-1), i , -1):
+		if(A[j] < A[j-1]):
+			A[j],A[j-1] = (A[j-1],A[j])
+print(A)
+
+
+#print(A[2])
+#print(A[-2])
+
