4a4a338c16c6839c08b5a3e36d757f53b8ea4149
swatistechnotes <63666256+swatistechnotes@users.noreply.github.com>
2020-04-16 03:36:52 +0530

    Add files via upload

---
+++ selectionsort.py
@@ -0,0 +1,9 @@
+
+A = list((5, 2, 1, 3, 4, 0))
+print(A)
+
+for i in range(len(A)-1):
+	min_position = A.index(min(A[i:]))
+	#print(min_position)
+	A[min_position], A[i] = A[i], A[min_position]
+print(A)
